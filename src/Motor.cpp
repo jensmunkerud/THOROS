be8e53c16e58d5e3ccd2e4226be0875f4a9fdfc5
Jens <34352934+jensmunkerud@users.noreply.github.com>
2026-04-15 20:35:32 +0200

    Removed velocity pid loop

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -1,26 +1,5 @@
 #include "Motor.h"
 
-static inline float degToRad(float deg) {
-	return deg * DEG_TO_RAD;
-}
-
-static void bodyAccelToWorldHorizontal(float axBody, float ayBody, float azBody, float rollDeg, float pitchDeg, float yawDeg, float& axWorld, float& ayWorld) {
-	const float roll = degToRad(rollDeg);
-	const float pitch = degToRad(pitchDeg);
-	const float yaw = degToRad(yawDeg);
-
-	const float cr = cosf(roll);
-	const float sr = sinf(roll);
-	const float cp = cosf(pitch);
-	const float sp = sinf(pitch);
-	const float cy = cosf(yaw);
-	const float sy = sinf(yaw);
-
-	// R_b2w = Rz(yaw) * Ry(pitch) * Rx(roll)
-	axWorld = (cy * cp) * axBody + (cy * sp * sr - sy * cr) * ayBody + (cy * sp * cr + sy * sr) * azBody;
-	ayWorld = (sy * cp) * axBody + (sy * sp * sr + cy * cr) * ayBody + (sy * sp * cr - cy * sr) * azBody;
-}
-
 Motor::Motor(MovementController& mc, Status& s) : 
 movementController{mc},
 status{s},
@@ -35,31 +14,15 @@ m4{0},
 pitchInput{0},
 rollInput{0},
 yawInput{0},
-velocityXInput{0},
-velocityYInput{0},
 quickPitchOUT{0},
 quickRollOUT{0},
 quickYawOUT{0},
-velocityXOUT{0},
-velocityYOUT{0},
 quickPitchCommand{0},
 quickRollCommand{0},
 quickYawCommand{0},
-velocityXTarget{0},
-velocityYTarget{0},
 pitchQuickPID{&pitchInput, &quickPitchOUT, &target.pitch, pitchPid.P, pitchPid.I, pitchPid.D, QuickPID::Action::direct},
 yawQuickPID{&yawInput, &quickYawOUT, &target.yaw, yawPid.P, yawPid.I, yawPid.D, QuickPID::Action::direct},
 rollQuickPID{&rollInput, &quickRollOUT, &target.roll, rollPid.P, rollPid.I, rollPid.D, QuickPID::Action::reverse},
-velocityXQuickPID{&velocityXInput, &velocityXOUT, &velocityXTarget, velocityXPid.P, velocityXPid.I, velocityXPid.D, QuickPID::Action::direct},
-velocityYQuickPID{&velocityYInput, &velocityYOUT, &velocityYTarget, velocityYPid.P, velocityYPid.I, velocityYPid.D, QuickPID::Action::direct},
-accelX{0},
-accelY{0},
-worldVelocityX{0},
-worldVelocityY{0},
-xVelocity{0},
-yVelocity{0},
-lastMotionUpdateMs{0},
-lastOuterLoopUs{0},
 lastInnerLoopUs{0}
 {}
 
@@ -72,23 +35,15 @@ void Motor::begin() {
 	pitchQuickPID.SetMode(QuickPID::Control::automatic);
 	rollQuickPID.SetMode(QuickPID::Control::automatic);
 	yawQuickPID.SetMode(QuickPID::Control::automatic);
-	velocityXQuickPID.SetMode(QuickPID::Control::automatic);
-	velocityYQuickPID.SetMode(QuickPID::Control::automatic);
 
 	pitchQuickPID.SetSampleTimeUs(ATTITUDE_PID_SAMPLE_US);
 	rollQuickPID.SetSampleTimeUs(ATTITUDE_PID_SAMPLE_US);
 	yawQuickPID.SetSampleTimeUs(ATTITUDE_PID_SAMPLE_US);
-	velocityXQuickPID.SetSampleTimeUs(VELOCITY_PID_SAMPLE_US);
-	velocityYQuickPID.SetSampleTimeUs(VELOCITY_PID_SAMPLE_US);
 
 	pitchQuickPID.SetOutputLimits(-PITCH_PID_OUTPUT_LIMIT, PITCH_PID_OUTPUT_LIMIT);
 	rollQuickPID.SetOutputLimits(-ROLL_PID_OUTPUT_LIMIT, ROLL_PID_OUTPUT_LIMIT);
 	yawQuickPID.SetOutputLimits(-YAW_PID_OUTPUT_LIMIT, YAW_PID_OUTPUT_LIMIT);
-	velocityXQuickPID.SetOutputLimits(-VELOCITY_PID_OUTPUT_LIMIT, VELOCITY_PID_OUTPUT_LIMIT);
-	velocityYQuickPID.SetOutputLimits(-VELOCITY_PID_OUTPUT_LIMIT, VELOCITY_PID_OUTPUT_LIMIT);
-	lastMotionUpdateMs = millis();
-	lastOuterLoopUs = micros();
-	lastInnerLoopUs = lastOuterLoopUs;
+	lastInnerLoopUs = micros();
 	
 	for(int i = 0; i < INITILIZE_ESC_TIME; i++) {
 		motor1.send_dshot_value(0);
@@ -100,7 +55,6 @@ void Motor::begin() {
 	status.motorArmed = 1;
 }
 
-
 void Motor::setAttitudePidTunings(const PID& pitch, const PID& roll, const PID& yaw) {
 	pitchPid = pitch;
 	rollPid = roll;
@@ -108,59 +62,6 @@ void Motor::setAttitudePidTunings(const PID& pitch, const PID& roll, const PID& 
 	pitchQuickPID.SetTunings(pitchPid.P, pitchPid.I, pitchPid.D);
 	rollQuickPID.SetTunings(rollPid.P, rollPid.I, rollPid.D);
 	yawQuickPID.SetTunings(yawPid.P, yawPid.I, yawPid.D);
-}
-
-
-// Estimate x y velocity from x y acceleration (Drone space)
-void Motor::updateMotionCorrection(float dtSeconds, float pidAuthority) {
-	if (dtSeconds <= 0.0f) {
-		return;
-	}
-
-	accelX = status.linearAccel.x;
-	accelY = status.linearAccel.y;
-
-	if (fabsf(accelX) < ACCEL_DEADBAND_G) {
-		accelX = 0.0f;
-	}
-	if (fabsf(accelY) < ACCEL_DEADBAND_G) {
-		accelY = 0.0f;
-	}
-
-	// Integrate horizontal acceleration in world frame, then project back to body frame.
-	float axWorld = 0.0f;
-	float ayWorld = 0.0f;
-	bodyAccelToWorldHorizontal(
-		accelX,
-		accelY,
-		accelRawZ,
-		status.attitude.roll,
-		status.attitude.pitch,
-		status.attitude.yaw,
-		axWorld,
-		ayWorld
-	);
-
-	worldVelocityX += axWorld * 9.81f * dt;
-	worldVelocityY += ayWorld * 9.81f * dt;
-
-	// Apply velocity decay to prevent drift
-	float decay = constrain(1.0f - VELOCITY_DECAY_PER_SEC * dt, 0.0f, 1.0f);
-	worldVelocityX *= decay;
-	worldVelocityY *= decay;
-
-	const float yaw = degToRad(status.attitude.yaw);
-	const float cy = cosf(yaw);
-	const float sy = sinf(yaw);
-	xVelocity = cy * worldVelocityX + sy * worldVelocityY;
-	yVelocity = -sy * worldVelocityX + cy * worldVelocityY;
-
-	if (fabsf(xVelocity) < VELOCITY_ZERO_CLAMP_MPS) {
-		xVelocity = 0.0f;
-	}
-	if (fabsf(yVelocity) < VELOCITY_ZERO_CLAMP_MPS) {
-		yVelocity = 0.0f;
-	}
 }
 
 
@@ -216,40 +117,23 @@ void Motor::loop() {
 	// }
 
 	unsigned long nowUs = micros();
-	bool runOuter = (lastOuterLoopUs == 0) || ((unsigned long)(nowUs - lastOuterLoopUs) >= (unsigned long)VELOCITY_PID_SAMPLE_US);
 	bool runInner = (lastInnerLoopUs == 0) || ((unsigned long)(nowUs - lastInnerLoopUs) >= (unsigned long)ATTITUDE_PID_SAMPLE_US);
-
-	float dtSeconds = 0.0f;
-	if (runOuter) {
-		unsigned long elapsedOuterUs = (lastOuterLoopUs == 0) ? 0 : (unsigned long)(nowUs - lastOuterLoopUs);
-		dtSeconds = elapsedOuterUs / 1000000.0f;
-		lastOuterLoopUs = nowUs;
-		lastMotionUpdateMs = millis();
-	}
+	const ControlInput controlInput = movementController.currentInput;
+	target.pitch = controlInput.pitch * ATTITUDE_COMMAND_SCALE;
+	target.roll = controlInput.roll * ATTITUDE_COMMAND_SCALE;
+	target.yaw = controlInput.yaw * ATTITUDE_COMMAND_SCALE;
 	
-	float throttleBase = movementController.currentInput.throttle + 350;
+	float throttleBase = controlInput.throttle + 350;
 	pidAuthority = constrain(
 			(throttleBase - (float)MINIMUM_MOTOR_SPEED) / (float)(PID_MAX_EFFECT_AFTER_SPEED - MINIMUM_MOTOR_SPEED),
 			0.0f,
 			1.0f
 	);
 
-	if (runOuter) {
-		// OUTER LOOP: Velocity Control
-		// Integrate acceleration to estimate velocity
-		updateMotionCorrection(dtSeconds, pidAuthority);
-
-		// Update velocity PIDs - these output target pitch and roll angles
-		// velocityXOUT controls the target roll for X velocity
-		// velocityYOUT controls the target pitch for Y velocity
-		updateAxisPid(velocityXQuickPID, velocityXTarget, xVelocity, velocityXInput, velocityXOUT, target.roll, VELOCITY_PID_OUTPUT_LIMIT, VELOCITY_I_BLEED_ZERO_ERROR, VELOCITY_I_BLEED_FULL_ERROR, VELOCITY_I_BLEED_MAX_PER_LOOP);
-		updateAxisPid(velocityYQuickPID, velocityYTarget, yVelocity, velocityYInput, velocityYOUT, target.pitch, VELOCITY_PID_OUTPUT_LIMIT, VELOCITY_I_BLEED_ZERO_ERROR, VELOCITY_I_BLEED_FULL_ERROR, VELOCITY_I_BLEED_MAX_PER_LOOP);
-	}
-
 	if (runInner) {
 		lastInnerLoopUs = nowUs;
 		// INNER LOOP: Attitude Stabilization
-		// Filter and control attitude to the targets set by velocity PIDs
+		// Filter and control attitude to the targets set by the movement controller
 		updateAxisPid(pitchQuickPID, target.pitch, status.attitude.pitch, pitchInput, quickPitchOUT, quickPitchCommand, PITCH_PID_OUTPUT_LIMIT, ATTITUDE_I_BLEED_ZERO_ERROR, ATTITUDE_I_BLEED_FULL_ERROR, ATTITUDE_I_BLEED_MAX_PER_LOOP);
 		updateAxisPid(rollQuickPID, target.roll, status.attitude.roll, rollInput, quickRollOUT, quickRollCommand, ROLL_PID_OUTPUT_LIMIT, ATTITUDE_I_BLEED_ZERO_ERROR, ATTITUDE_I_BLEED_FULL_ERROR, ATTITUDE_I_BLEED_MAX_PER_LOOP);
 		updateAxisPid(yawQuickPID, target.yaw, status.attitude.yaw, yawInput, quickYawOUT, quickYawCommand, YAW_PID_OUTPUT_LIMIT, ATTITUDE_I_BLEED_ZERO_ERROR, ATTITUDE_I_BLEED_FULL_ERROR, ATTITUDE_I_BLEED_MAX_PER_LOOP);
@@ -260,10 +144,7 @@ void Motor::loop() {
 	float rollCmd = quickRollCommand * pidAuthority;
 	float yawCmd = quickYawCommand * pidAuthority;
 
-	// Apply only the velocity loop mix (outer loop output only)
-	// float pitchCmd = target.pitch * pidAuthority;
-	// float rollCmd = - target.roll * pidAuthority;
-	// float yawCmd = 0.0f;
+	// Apply the attitude PID outputs to the motor mix.
 	
 	// Apply motor mix
 	m1 = constrain(throttleBase * FRONT_BIAS + pitchCmd - rollCmd - yawCmd, 0, MAXIMUM_MOTOR_SPEED);
@@ -276,7 +157,6 @@ void Motor::loop() {
 	motor3.send_dshot_value((int)m3);
 	motor4.send_dshot_value((int)m4);
 	
-	Serial.println(xVelocity, 8);
 	return;
 	Serial.print(status.attitude.pitch);
 	Serial.print("/");
--- src/Motor.h
+++ src/Motor.h
@@ -15,24 +15,17 @@ constexpr int INITILIZE_ESC_TIME (40); // 4000
 constexpr int MINIMUM_MOTOR_SPEED (70);
 constexpr int MAXIMUM_MOTOR_SPEED (1000);
 constexpr int PID_MAX_EFFECT_AFTER_SPEED (200);
-constexpr int VELOCITY_PID_SAMPLE_US (10000);	// Outer PID loop
 constexpr int ATTITUDE_PID_SAMPLE_US (1000);	// Inner PID loop
 constexpr int PITCH_PID_OUTPUT_LIMIT (200);
 constexpr int YAW_PID_OUTPUT_LIMIT (200);
 constexpr int ROLL_PID_OUTPUT_LIMIT (200);
-constexpr int VELOCITY_PID_OUTPUT_LIMIT (100);
+constexpr float ATTITUDE_COMMAND_SCALE (0.05f);
 constexpr float AXIS_INPUT_LPF_ALPHA (0.05f);
 constexpr float AXIS_OUTPUT_SLEW_PER_LOOP (12.0f);
 constexpr float ATTITUDE_I_BLEED_ZERO_ERROR (3.0f);
 constexpr float ATTITUDE_I_BLEED_FULL_ERROR (8.0f);
 constexpr float ATTITUDE_I_BLEED_MAX_PER_LOOP (0.01f);
-constexpr float VELOCITY_I_BLEED_ZERO_ERROR (0.05f);
-constexpr float VELOCITY_I_BLEED_FULL_ERROR (0.50f);
-constexpr float VELOCITY_I_BLEED_MAX_PER_LOOP (0.02f);
 constexpr float FRONT_BIAS (1.1f);
-constexpr float ACCEL_DEADBAND_G (0.0f);
-constexpr float VELOCITY_ZERO_CLAMP_MPS (0.02f);
-constexpr float VELOCITY_DECAY_PER_SEC (0.001f);
 
 constexpr dshot_mode_e DSHOT_TYPE{DSHOT300};
 
@@ -45,8 +38,6 @@ public:
 	PID pitchPid{8, 4, 0.7};
 	PID yawPid{2, 4, 1};
 	PID rollPid{5, 4, 0.6};
-	PID velocityXPid{5, 0, 30};
-	PID velocityYPid{5, 0, 30};
 
 private:
 	MovementController& movementController;
@@ -60,41 +51,23 @@ private:
 	QuickPID pitchQuickPID;
 	QuickPID rollQuickPID;
 	QuickPID yawQuickPID;
-	QuickPID velocityXQuickPID;
-	QuickPID velocityYQuickPID;
 	float computeIntegralBleed(float errorAbs, float zeroError, float fullError, float maxBleedPerLoop) const;
 	void updateAxisPid(QuickPID& pid, float setpoint, float measurement, float& filteredInput, float& pidOutput, float& command, float outputLimit, float iBleedZeroError, float iBleedFullError, float iBleedMaxPerLoop);
-	void updateMotionCorrection(float dtSeconds, float pidAuthority);
 	float pidAuthority;
 
-	// Make these next 7 part of some "tilstandsvariabel"
+	// Filtered control and attitude state used by the attitude loop.
 	float pitchInput;
 	float yawInput;
 	float rollInput;
-	float velocityXInput;
-	float velocityYInput;
-	float accelX;
-	float accelY;
-	float worldVelocityX;
-	float worldVelocityY;
-	float xVelocity;
-	float yVelocity;
-	
-	unsigned long lastMotionUpdateMs;
-	unsigned long lastOuterLoopUs;
 	unsigned long lastInnerLoopUs;
 
 	float quickPitchOUT;
 	float quickYawOUT;
 	float quickRollOUT;
-	float velocityXOUT;
-	float velocityYOUT;
 
 	float quickPitchCommand;
 	float quickYawCommand;
 	float quickRollCommand;
-	float velocityXTarget;
-	float velocityYTarget;
 
 	double m1;
 	double m2;
